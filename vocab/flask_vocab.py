f4f91a30eb2e1a3ce386863012e80b24c411197f
JoshuaJordal <Joshuajordal@gmail.com>
2023-02-07 01:22:56 -0800

    finished

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -6,6 +6,7 @@ from a scrambled string)
 
 import flask
 import logging
+from flask import request
 
 # Our modules
 from src.letterbag import LetterBag
@@ -79,7 +80,7 @@ def success():
 #   a JSON request handler
 #######################
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check")
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -92,7 +93,8 @@ def check():
     app.logger.debug("Entering check")
 
     # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
+    #text = flask.request.form["attempt"]
+    text = request.args.get("text", type=str)
     jumble = flask.session["jumble"]
     matches = flask.session.get("matches", [])  # Default to empty list
 
@@ -105,22 +107,24 @@ def check():
         # Cool, they found a new word
         matches.append(text)
         flask.session["matches"] = matches
+        if len(matches) >= flask.session["target_count"]:
+            rslt = {"text": "You found {}".format(text), "matched": True, "matches": matches, "redirect": True}
+            return flask.jsonify(result=rslt)
+        
+        rslt = {"text": "You found {}".format(text), "matched": True, "matches": matches}
+        return flask.jsonify(result=rslt)
     elif text in matches:
-        flask.flash("You already found {}".format(text))
+        rslt = {"text": "You already found {}".format(text), "matched": False}
+        return flask.jsonify(result=rslt)
     elif not matched:
-        flask.flash("{} isn't in the list of words".format(text))
+        rslt = {"text": "{} isn't in the list of words".format(text), "matched": False}
+        return flask.jsonify(result=rslt)
     elif not in_jumble:
-        flask.flash(
-            '"{}" can\'t be made from the letters {}'.format(text, jumble))
+        rslt = {"text": '"{}" can\'t be made from the letters {}'.format(text, jumble), "matched": False}
+        return flask.jsonify(result=rslt)
     else:
         app.logger.debug("This case shouldn't happen!")
         assert False  # Raises AssertionError
-
-    # Choose page:  Solved enough, or keep going?
-    if len(matches) >= flask.session["target_count"]:
-       return flask.redirect(flask.url_for("success"))
-    else:
-       return flask.redirect(flask.url_for("keep_going"))
 
 
 ###############
